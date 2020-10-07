ef3594cd1ff68b1827ec68110f300878b1d6e25b
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-07 13:35:48 -0700

    added a footer section

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar/Navbar';
 import MainContent from './MainContent/MainContent'
+import Footer from './Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainContent />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
