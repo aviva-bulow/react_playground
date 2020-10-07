2c895f4e6d5ad0c3f05f6b261cca694dc83eb913
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-07 13:55:21 -0700

    added a todolist

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutMe from "./AboutMe";
+import ToDoList from "./ToDoList";
 
 function MainContent() {
     return <dev>
         <header className="MainContent-header">
             <AboutMe />
+            <ToDoList />
         </header>
     </dev>
 
---
+++ src/components/MainContent/ToDoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ToDoList() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>placeholder</p>
+
+            <input type="checkbox" />
+            <p>placeholder</p>
+
+            <input type="checkbox" />
+            <p>placeholder</p>
+        </div>
+    )
+}
+
+export default ToDoList;
