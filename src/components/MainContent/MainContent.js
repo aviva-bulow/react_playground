91e4325a4f8ef3ec5ba338e179607fc8e877d39d
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-08 15:34:46 -0700

    moved todo list to be next to about section

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -2,14 +2,23 @@ import React from 'react';
 import AboutMe from "./AboutMe";
 import ToDoList from "./ToDoList";
 import "./MainContent.css"
+import { Card, CardColumns } from 'react-bootstrap';
 
 function MainContent() {
 
-    return (
-        <header className="MainContent-header">
-            <AboutMe />
-            <ToDoList />
-        </header>)
+  return (
+    <header className="MainContent-header">
+      <CardColumns>
+        <Card>
+          <AboutMe />
+        </Card>
+
+        <Card>
+          <ToDoList />
+        </Card>
+
+      </CardColumns>
+    </header>)
 
 }
 
