80f8c8faa25935c18673e775dd6e56119b553c31
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-08 14:58:20 -0700

    delete unused css file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import MyNavbar from './Navbar/Navbar';
 import MainContent from './MainContent/MainContent'
 import Footer from './Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <MyNavbar />
       <MainContent />
       <Footer />
