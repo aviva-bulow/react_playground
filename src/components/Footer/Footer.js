b464adc2f11f73d1f8235dcaa03e980531f1b8ab
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-12 16:28:16 -0700

    removed containing container

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import './Footer.css'
 
 function Footer() {
-  return <Container>
-    <Navbar fixed="bottom" expand="lg" variant="dark" bg="dark">
-      <Navbar.Text>
-        This is the footer.
+  return <Navbar expand="lg" variant="dark" bg="dark">
+    <Navbar.Text>
+      This is the footer.
       </Navbar.Text>
-    </Navbar>
-  </Container>
+  </Navbar>
 }
 
 export default Footer;
