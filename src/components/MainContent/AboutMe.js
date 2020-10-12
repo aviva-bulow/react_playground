d2bea869873617320b6739ebae95bd6da09988f9
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-12 13:14:42 -0700

    changing main content

--- src/components/MainContent/AboutMe.js
+++ src/components/MainContent/AboutMe.js
@@ -9,8 +9,8 @@ function AboutMe() {
     <div className="AboutMe">
       <Card.Body >
         <Photo />
-        <Hobbies />
-        <LinkedIn />
+        <MyTitle />
+        <ProfessionalSummary />
       </Card.Body>
     </div >
   )
@@ -24,37 +24,26 @@ function Photo() {
   )
 }
 
-
-function Hobbies() {
-  return (
-    <div>
-      <a id="hobbies"></a>
-
-      <Card.Title>Hobbies</Card.Title>
-      <Card.Text>
-        <ul>
-          <li>Running</li>
-          <li>Backpacking</li>
-          <li>Gardening</li>
-        </ul>
-      </Card.Text>
-    </div>
-  )
+function MyTitle() {
+  return <Card.Title>
+    Entrepreneur & Software Engineer
+  </Card.Title>
 }
 
-function LinkedIn() {
-  return (
-    <Card.Text>
-      <a
-        className="AboutMe-link"
-        href="https://www.linkedin.com/in/aviva-bulow/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Linked In
-            </a>
-    </Card.Text>
-  )
+function ProfessionalSummary() {
+  return <Card.Text>
+    A software engineer with 6 years experience,
+    primarily developing back-end systems in Python,
+    I’m currently learning front-end development in
+    JavaScript/HTML with React+Bootstrap.
+    As an analytical thinker with a background in mathematics,
+    I enjoy tackling difficult problems and exploring new ideas.
+    I’m highly effective at communicating complex
+    concepts across knowledge barriers.
+  </Card.Text>
 }
+
+
+
 
 export default AboutMe;
