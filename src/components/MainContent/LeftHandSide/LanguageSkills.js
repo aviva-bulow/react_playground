9ab25d0efef6434d323d82f36e601e3725c5984c
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-13 14:22:30 -0700

    add language skill component

--- src/components/MainContent/LeftHandSide/LanguageSkills.js
+++ src/components/MainContent/LeftHandSide/LanguageSkills.js
@@ -1,11 +1,14 @@
+import { DatePicker } from "antd";
 import React from "react";
-import { Card, Container, ProgressBar, Row, Col } from "react-bootstrap";
+import { Card, Container, ProgressBar, Row, Col, } from "react-bootstrap";
 import AccordionComponent from "../AccordionComponent";
 
 function LanguageSkills() {
   return <div>
     <LanguageSkill name="Python" skillLevel="90" libraries={["multiprocessing", "unittest", "vpython", "numpy", "Spacy"]} />
     {/* <LanguageSkill name="JavaScript" skillLevel="40" /> */}
+    <LanguageSkill name="Java" skillLevel="65" libraries={["awt", "swing"]} />
+
   </div>
 }
 
@@ -13,13 +16,12 @@ function LanguageSkill(props) {
   console.log(props.name)
   // props.libraries
   console.log(props.skillLevel > 10)
-  let title = (< Container >
-    <Row>
-
-      <Col><Card.Title>{props.name}</Card.Title></Col>
-      <Col><ProgressBar variant="success" now={props.skillLevel} label={`${props.skillLevel}%`} /></Col>
+  let title = (<Container>
+    <Row >
+      <Col ><Card.Text>{props.name}</Card.Text></Col>
+      <Col ><ProgressBar variant="success" now={props.skillLevel} label={`${props.skillLevel}%`} /></Col>
     </Row>
-  </Container >
+  </Container>
   )
   return <AccordionComponent title={title} body={props.libraries.map(LanguageLibrary)} />
 }
