e2b582fd6cd31668b74b84ea216b41d78a59f51d
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-13 18:56:39 -0700

    add computing, math, and science skills

--- src/components/MainContent/LeftHandSide/Skills.js
+++ src/components/MainContent/LeftHandSide/Skills.js
@@ -1,16 +1,15 @@
 import React from "react";
-import AccordionComponent from "../AccordionComponent"
 import LanguageSkills from "./LanguageSkills"
-
+import OtherSkillGroups from "./OtherSkillGroups"
 
 function Skills() {
 
 
   return <div>
     <LanguageSkills />
-    {/* <ComputingSkills /> */}
-    {/* <MathSkills /> */}
-    {/* <ScienceSkills /> */}
+    <OtherSkillGroups title="Computing Skills" skillList={["Object Oriented Design", "Unit Testing", "Complexity Analysis", "Parallel Computing", "Learning Languages"]} />
+    <OtherSkillGroups title="Mathematical Talents" skillList={["Probability and Statistics", "Algorithm Analysis", "Graph Theory", "Proof Construction and Validation"]} />
+    <OtherSkillGroups title="Science" skillList={["Experiment Design", "Data Interpretation", "Computational Modeling", "Data Visualization"]} />
     {/* <LeadershipSkills /> */}
   </div>
 
