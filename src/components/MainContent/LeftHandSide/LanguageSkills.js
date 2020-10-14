e8553761e66e4d00b32e3e73ecc4a8f64297d355
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-14 15:01:39 -0700

    formatting with prettier

--- src/components/MainContent/LeftHandSide/LanguageSkills.js
+++ src/components/MainContent/LeftHandSide/LanguageSkills.js
@@ -3,39 +3,50 @@ import { Row, Col, Progress } from "antd";
 import AccordionComponent from "../AccordionComponent";
 
 function LanguageSkills() {
-  let body = (<div>
-    <LanguageSkill name="Python" skillLevel="90" libraries={["multiprocessing", "unittest", "vpython", "numpy", "Spacy"]} />
-    {/* <LanguageSkill name="JavaScript" skillLevel="40" /> */}
-    <LanguageSkill name="C" skillLevel="65" libraries={["MPI", "OpenMp"]} />
-    <LanguageSkill name="Java" skillLevel="65" libraries={["awt", "swing"]} />
+  let body = (
+    <div>
+      <LanguageSkill
+        name="Python"
+        skillLevel="90"
+        libraries={["multiprocessing", "unittest", "vpython", "numpy", "Spacy"]}
+      />
+      {/* <LanguageSkill name="JavaScript" skillLevel="40" /> */}
+      <LanguageSkill name="C" skillLevel="65" libraries={["MPI", "OpenMp"]} />
+      <LanguageSkill name="Java" skillLevel="65" libraries={["awt", "swing"]} />
 
-    <LanguageSkill name="R" skillLevel="65" libraries={["ggplot2"]} />
-    <LanguageSkill name="SQLite" skillLevel="50" libraries={[]} />
-  </div>)
+      <LanguageSkill name="R" skillLevel="65" libraries={["ggplot2"]} />
+      <LanguageSkill name="SQLite" skillLevel="50" libraries={[]} />
+    </div>
+  );
 
-  return <AccordionComponent title="Programming Languages" body={body} />
+  return <AccordionComponent title="Programming Languages" body={body} />;
 }
 
 function LanguageSkill(props) {
-  console.log(props.name)
+  console.log(props.name);
   // props.libraries
-  console.log(props.skillLevel > 10)
+  console.log(props.skillLevel > 10);
 
   let title = (
-    <Row >
-      <Col span={8}><p>{props.name}</p></Col>
+    <Row>
+      <Col span={8}>
+        <p>{props.name}</p>
+      </Col>
       <Col span={13} offset={3}>
-
-        <Progress percent={props.skillLevel} size="small" strokeColor='green' />
+        <Progress percent={props.skillLevel} size="small" strokeColor="green" />
       </Col>
     </Row>
-  )
-  return <AccordionComponent title={title} body={props.libraries.map(LanguageLibrary)} />
+  );
+  return (
+    <AccordionComponent
+      title={title}
+      body={props.libraries.map(LanguageLibrary)}
+    />
+  );
 }
 
 function LanguageLibrary(name) {
-  return <p>{name}</p>
-
+  return <p>{name}</p>;
 }
 
 export default LanguageSkills;
--- src/components/MainContent/LeftHandSide/Skills.js
+++ src/components/MainContent/LeftHandSide/Skills.js
@@ -1,20 +1,50 @@
 import React from "react";
-import LanguageSkills from "./LanguageSkills"
-import OtherSkillGroups from "./OtherSkillGroups"
+import LanguageSkills from "./LanguageSkills";
+import OtherSkillGroups from "./OtherSkillGroups";
 
 function Skills() {
-
-
-  return <div>
-    <LanguageSkills />
-    <OtherSkillGroups title="Computing Skills" skillList={["Object Oriented Design", "Unit Testing", "Complexity Analysis", "Parallel Computing", "Learning Languages"]} />
-    <OtherSkillGroups title="Mathematical Talents" skillList={["Probability and Statistics", "Algorithm Analysis", "Graph Theory", "Proof Construction and Validation"]} />
-    <OtherSkillGroups title="Scientific Know-how" skillList={["Experiment Design", "Data Interpretation", "Computational Modeling", "Data Visualization"]} />
-    <OtherSkillGroups title="Leadership Abilities" skillList={["Communication", "Management", "Organization", "Innovation & Design"]} />
-  </div>
-
+  return (
+    <div>
+      <LanguageSkills />
+      <OtherSkillGroups
+        title="Computing Skills"
+        skillList={[
+          "Object Oriented Design",
+          "Unit Testing",
+          "Complexity Analysis",
+          "Parallel Computing",
+          "Learning Languages",
+        ]}
+      />
+      <OtherSkillGroups
+        title="Mathematical Talents"
+        skillList={[
+          "Probability and Statistics",
+          "Algorithm Analysis",
+          "Graph Theory",
+          "Proof Construction and Validation",
+        ]}
+      />
+      <OtherSkillGroups
+        title="Scientific Know-how"
+        skillList={[
+          "Experiment Design",
+          "Data Interpretation",
+          "Computational Modeling",
+          "Data Visualization",
+        ]}
+      />
+      <OtherSkillGroups
+        title="Leadership Abilities"
+        skillList={[
+          "Communication",
+          "Management",
+          "Organization",
+          "Innovation & Design",
+        ]}
+      />
+    </div>
+  );
 }
-
-
 
 export default Skills;
