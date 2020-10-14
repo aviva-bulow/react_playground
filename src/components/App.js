de767ae79e7d60644e53fffdd2a43e5ebe7b1031
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-13 17:13:28 -0700

    discard footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header/Header';
 import MainContent from './MainContent/MainContent'
-import Footer from './Footer/Footer'
 
 function App() {
   return (
