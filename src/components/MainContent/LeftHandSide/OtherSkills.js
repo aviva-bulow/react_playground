ac593324d199e851db0c30152a683a699cd393e3
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-15 16:28:30 -0700

    convert to class component

--- src/components/MainContent/LeftHandSide/OtherSkills.js
+++ src/components/MainContent/LeftHandSide/OtherSkills.js
@@ -3,16 +3,18 @@ import AccordionComponent from "../AccordionComponent";
 import otherSkillsData from "./OtherSkillsData";
 import Skill from "./Skill";
 
-function OtherSkills(props) {
-  return otherSkillsData.map((skillClass) => (
-    <AccordionComponent
-      title={skillClass.title}
-      body={skillClass.skillList.map((skillName) => (
-        <Skill name={skillName} key={skillName} />
-      ))}
-      key={skillClass.title}
-    />
-  ));
+class OtherSkills extends React.Component {
+  render() {
+    return otherSkillsData.map((skillClass) => (
+      <AccordionComponent
+        title={skillClass.title}
+        body={skillClass.skillList.map((skillName) => (
+          <Skill name={skillName} key={skillName} />
+        ))}
+        key={skillClass.title}
+      />
+    ));
+  }
 }
 
 export default OtherSkills;
