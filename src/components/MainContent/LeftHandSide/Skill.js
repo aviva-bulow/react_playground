f6fe7eb3f2686c0d69a4cfd2cac5780887e8e619
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-15 16:25:28 -0700

    convert to class based component

--- src/components/MainContent/LeftHandSide/Skill.js
+++ src/components/MainContent/LeftHandSide/Skill.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Card } from "antd";
 
-function Skill(skillName) {
-  return (
-    <Card size="small">
-      <p>{skillName}</p>
-    </Card>
-  );
+class Skill extends React.Component {
+  render() {
+    return (
+      <Card size="small">
+        <p>{this.props.name}</p>
+      </Card>
+    );
+  }
 }
 
 export default Skill;
