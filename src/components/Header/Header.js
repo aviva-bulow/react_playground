2323c31ecb94e2f7f6a740d5d492af29bf65de64
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-15 17:05:16 -0700

    convert to class based components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function Header() {
-  const date = new Date()
-  let hours = date.getHours()
-  let msg = ""
-  if (11 <= hours && hours < 15) {
-    msg = "Good day!"
-  } else if (15 <= hours && hours < 23) {
-    msg = "Good Afternoon"
-  } else if (23 <= hours || hours < 3) {
-    msg = "Good Night"
-  } else {
-    msg = "Good Morning!"
+class Header extends React.Component {
+  render() {
+    const date = new Date();
+    let hours = date.getHours();
+    let msg = "";
+    if (11 <= hours && hours < 15) {
+      msg = "Good day!";
+    } else if (15 <= hours && hours < 23) {
+      msg = "Good Afternoon";
+    } else if (23 <= hours || hours < 3) {
+      msg = "Good Night";
+    } else {
+      msg = "Good Morning!";
+    }
+    return (
+      <div>
+        <a id="home"></a>
+        <Navbar sticky="top" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Aviva Bulow</Navbar.Brand>
+
+          <Nav className="mr-auto">
+            <Nav.Link href="#home"> Home</Nav.Link>
+            <Nav.Link href="#professional_summary">
+              Professional Summary
+            </Nav.Link>
+          </Nav>
+
+          <Navbar.Text>{msg}</Navbar.Text>
+        </Navbar>
+      </div>
+    );
   }
-  return (
-    <div>
-      <a id="home"></a>
-      <Navbar sticky="top" bg="dark" variant="dark" >
-        <Navbar.Brand href="#home">
-          Aviva Bulow
-        </Navbar.Brand>
-
-        <Nav className="mr-auto">
-          <Nav.Link href="#home"> Home</Nav.Link>
-          <Nav.Link href="#professional_summary">Professional Summary</Nav.Link>
-        </Nav>
-
-        <Navbar.Text>
-          {msg}
-        </Navbar.Text>
-
-      </Navbar>
-    </div >
-  )
 }
 
 export default Header;
