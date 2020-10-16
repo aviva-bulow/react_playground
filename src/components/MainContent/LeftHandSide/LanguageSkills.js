fb7952ba8a599a08c4bdc452e3f925bae4b56c9b
Aviva Bulow <aviva.bulow@gmail.com>
2020-10-15 17:02:59 -0700

    remove accordion component

--- src/components/MainContent/LeftHandSide/LanguageSkills.js
+++ src/components/MainContent/LeftHandSide/LanguageSkills.js
@@ -1,50 +1,56 @@
 import React from "react";
-import { Row, Col, Progress } from "antd";
-import AccordionComponent from "../AccordionComponent";
+import { Row, Col, Progress, Collapse } from "antd";
 import languageSkillsData from "./LanguageSkillsData";
 
-function LanguageSkills() {
-  return (
-    <AccordionComponent
-      title="Programming Languages"
-      body={languageSkillsData.map((languageSkill) => (
-        <LanguageSkill
-          name={languageSkill.name}
-          key={languageSkill.name}
-          skillLevel={languageSkill.skillLevel}
-          libraries={languageSkill.libraries}
-        />
-      ))}
-    />
-  );
+class LanguageSkills extends React.Component {
+  render() {
+    return (
+      <Collapse>
+        <Collapse.Panel header="Programming Languages">
+          {languageSkillsData.map((languageSkill) => (
+            <LanguageSkill
+              name={languageSkill.name}
+              key={languageSkill.name}
+              skillLevel={languageSkill.skillLevel}
+              libraries={languageSkill.libraries}
+            />
+          ))}
+        </Collapse.Panel>
+      </Collapse>
+    );
+  }
 }
 
-function LanguageSkill(props) {
-  console.log(props.name);
-  // props.libraries
-  console.log(props.skillLevel > 10);
-
-  let title = (
-    <Row>
-      <Col span={8}>
-        <p>{props.name}</p>
-      </Col>
-      <Col span={13} offset={3}>
-        <Progress percent={props.skillLevel} size="small" strokeColor="green" />
-      </Col>
-    </Row>
-  );
-  // if (props.libraries.length < 1) {
-  //   return <Skill name={title} />;
-  // }
-  return (
-    <AccordionComponent
-      title={title}
-      body={props.libraries.map((libName) => (
-        <p>{libName}</p>
-      ))}
-    />
-  );
+class LanguageSkill extends React.Component {
+  render() {
+    let title = (
+      <Row>
+        <Col span={8}>
+          <p>{this.props.name}</p>
+        </Col>
+        <Col span={13} offset={3}>
+          <Progress
+            percent={this.props.skillLevel}
+            size="small"
+            strokeColor="green"
+          />
+        </Col>
+      </Row>
+    );
+    let showArrow = true;
+    // if (this.props.libraries.length < 1) {
+    //   showArrow = false;
+    // }
+    return (
+      <Collapse>
+        <Collapse.Panel header={title} showArrow={showArrow}>
+          {this.props.libraries.map((libName) => (
+            <p>{libName}</p>
+          ))}
+        </Collapse.Panel>
+      </Collapse>
+    );
+  }
 }
 
 export default LanguageSkills;
--- src/components/MainContent/LeftHandSide/OtherSkills.js
+++ src/components/MainContent/LeftHandSide/OtherSkills.js
@@ -1,19 +1,21 @@
+import { Collapse } from "antd";
 import React from "react";
-import AccordionComponent from "../AccordionComponent";
 import otherSkillsData from "./OtherSkillsData";
 import Skill from "./Skill";
 
 class OtherSkills extends React.Component {
   render() {
-    return otherSkillsData.map((skillClass) => (
-      <AccordionComponent
-        title={skillClass.title}
-        body={skillClass.skillList.map((skillName) => (
-          <Skill name={skillName} key={skillName} />
+    return (
+      <Collapse>
+        {otherSkillsData.map((skillClass) => (
+          <Collapse.Panel header={skillClass.title} key={skillClass.title}>
+            {skillClass.skillList.map((skillName) => (
+              <Skill name={skillName} key={skillName} />
+            ))}
+          </Collapse.Panel>
         ))}
-        key={skillClass.title}
-      />
-    ));
+      </Collapse>
+    );
   }
 }
 
